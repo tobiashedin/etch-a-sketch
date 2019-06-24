a90555436846c48bec9c6dad90f9b1709b319c70
tobiashedin <tobias.hedin@gmail.com>
2019-06-24 14:49:09 +0200

    worked the grid

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-for (let x = 0; x <= 16; x++) {
-  console.log(x);
-}
+$(document).ready(function() {
+  for (let x = 0; x <= 16; x++) {
+    for (let y = 0; y <= 16; y++) {
+      let grid = $("<div class='grid'></div>");
+      grid.appendTo(".container");
+      console.log(x, y);
+    }
+  }
+});
