f25be135e698e11603a2ac6e85d36d97740e1f7c
tobiashedin <tobias.hedin@gmail.com>
2019-07-01 16:58:09 +0200

    partly fixed the grid

--- js/main.js
+++ js/main.js
@@ -1,49 +1,25 @@
-const gridSize = 16;
-const container = document.querySelector(".container");
+$(document).ready(function() {
+  createGrid(16);
+});
 
-function createGrid() {
-  for (let x = 1; x <= 16; x++) {
-    for (let y = 1; y <= 16; y++) {
-      let grid = $("<div class='grid'></div>");
-      grid.appendTo(".container");
-      console.log(x, y);
+function createGrid(x) {
+  for (let rows = 0; rows < x; rows++) {
+    for (let columns = 0; columns < x; columns++) {
+      $("#gridContainer").append("<div class='grid'></div>");
+    }
+  }
+  $(".grid").width(960 / x);
+  $(".grid").height(960 / x);
+}
+
+/*
+function appendDivs(container, rows, columns) {
+   const totalGridNum = rows * columns;
+  for (let x = 0; x < totalGridNum; x++) {
+    for (let y = 0; y < totalGridNum; y++) {
+      let div = document.createElement("div");
+      container.appendChild(div);
     }
   }
 }
-createGrid();
-/* for (let i = 0; i <= 16; i++) {
-    //let gridBox = document.createElement("div");
-    let gridBox = $("<div class='grid'></div>");
-    grid.appendTo(".contatiner");
-    //gridBox.classList.add("sqr");
-    //gridBox.style.border = "1px solid gray";
-    //container.appendChild(gridBox);
-  }
-  container.style.cssText =
-    "grid-template-columns: repeat(" +
-    num +
-    ", 1fr); grid-template-rows: repeat(" +
-    num +
-    ", 1fr)";
-} */
-
-//-----------------------------------------
-
-//$(document).ready(function() {});
-/* for (let i = 0; i <= 16; x++) {
-    for (let y = 0; (i = size ** 2); i++) {
-      let gridBox = $("<div class='grid'></div>");
-      grid.appendTo(".contatiner");
-      console.log(x, y);
-    }
-  } */
-
-/*   $(document).ready(function() {
-    for (let x = 0; x <= 16; x++) {
-      for (let y = 0; y <= 16; y++) {
-        let grid = $("<div class='grid'></div>");
-        grid.appendTo(".container");
-        console.log(x, y);
-      }
-    }
-  }); */
+createGrid(); */
