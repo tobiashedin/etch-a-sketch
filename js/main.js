e0d70406d89eed7892e6a0479f1a9bbd8d21e253
tobiashedin <tobias.hedin@gmail.com>
2019-07-03 13:48:35 +0200

    text position

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
+$(document).ready(function() {
+  createGrid(24);
+
+  $(".grid-div").mouseover(function() {
+    $(this).css("background-color", "black");
+  });
+});
+
 function createGrid(x) {
   for (let rows = 0; rows < x; rows++) {
     for (let columns = 0; columns < x; columns++) {
       $("#container").append("<div class='grid-div'></div>");
     }
   }
-  $(".grid-div").width(800 / x);
-  $(".grid-div").height(800 / x);
+  $(".grid-div").width(600 / x);
+  $(".grid-div").height(600 / x);
 }
-
-$(document).ready(function() {
-  createGrid(16);
-
-  $(".grid-div").mouseover(function() {
-    $(this).css("background-color", "gray");
-  });
-});
