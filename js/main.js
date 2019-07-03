848b04a93f682c60eac8e129ce20b61857b818cd
tobiashedin <tobias.hedin@gmail.com>
2019-07-03 11:16:50 +0200

    added buttons, destroyed grid...

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function createGrid(x) {
   for (let rows = 0; rows < x; rows++) {
     for (let columns = 0; columns < x; columns++) {
-      $("#gridContainer").append("<div class='grid'></div>");
+      $("#container").append("<div class='grid-div'></div>");
     }
   }
-  $(".grid").width(800 / x);
-  $(".grid").height(800 / x);
+  $(".grid-div").width(800 / x);
+  $(".grid-div").height(800 / x);
 }
 
 $(document).ready(function() {
   createGrid(16);
 
-  $(".grid").mouseover(function() {
-    $(this).css("background-color", "black");
+  $(".grid-div").mouseover(function() {
+    $(this).css("background-color", "gray");
   });
 });
